b5cd53bdc7d7f59589a20cb350fb3a23f22e2f2b
Asmae Hariti <haritiasmaa74@gmail.com>
2023-03-21 11:20:08 +0000

    changing strings of options to a tuple

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ import "./style.css";
 import { Elm } from "./src/Main.elm";
 
 if (process.env.NODE_ENV === "development") {
-    const ElmDebugTransform = await import("elm-debug-transformer")
+  const ElmDebugTransform = await import("elm-debug-transformer");
 
-    ElmDebugTransform.register({
-        simple_mode: true
-    })
+  ElmDebugTransform.register({
+    simple_mode: true,
+  });
 }
 
 const root = document.querySelector("#app div");
+// const flags = Json.parse(localStorage.getItem("questions")) || {};
+// console.log("initilizing app with flags: ", flags);
 const app = Elm.Main.init({ node: root });
+// app.ports.saveModel.subscribe(function (model) {
+//   console.log("saving model...");
+//   localStorage.setItem("questions", JSON.stringify(model));
+// });
--- src/Main.elm
+++ src/Main.elm
@@ -4,33 +4,32 @@ import Browser
 import Html exposing (Html, button, div, input, label, text)
 import Html.Attributes exposing (class, for, id, name, src, style, type_)
 import Html.Events exposing (onClick)
-import Json.Decode as Decode
+import Json.Decode as Decode exposing (Value)
 import Json.Encode as Encode
 import Msg exposing (Msg(..))
 
 
 type Msg
-    = MakeQuiz String
-    | AddQuestion String
-    | GetQuiz String
+    = MakeQuiz
+    | GetQuiz
 
 
 type alias Question =
     { text : String
-    , options : List String
-    , correctAnswer : Int
+    , options : List ( String, String )
+    , correctAnswer : String
     }
 
 
 questions : List Question
 questions =
     [ { text = "What is the capital of France?"
-      , options = [ "Paris", "Madrid", "Rome", "Berlin" ]
-      , correctAnswer = 1
+      , options = [ ( "A", "Paris" ), ( "B", "Madrid" ), ( "C", "Rome" ), ( "C", "Berlin" ) ]
+      , correctAnswer = "A"
       }
     , { text = "What is the largest organ in the human body?"
-      , options = [ "Heart", "Lungs", "Liver", "Skin" ]
-      , correctAnswer = 3
+      , options = [ ( "A", "Heart" ), ( "B", "Lungs" ) ]
+      , correctAnswer = "B"
       }
 
     -- add more questions
@@ -45,24 +44,22 @@ type alias Model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        MakeQuiz name ->
+        MakeQuiz ->
+            -- ( model, Cmd.none )
             let
                 newQuestion =
-                    { text = name
-                    , options = [ "Option A", "Option B", "Option C" ]
-                    , correctAnswer = 0
+                    { text = ""
+                    , options = []
+                    , correctAnswer = ""
                     }
             in
             ( { model | questions = newQuestion :: model.questions }, Cmd.none )
 
-        AddQuestion _ ->
-            ( model, Cmd.none )
-
-        GetQuiz name ->
+        GetQuiz ->
             ( model, Cmd.none )
 
 
-main : Program () Model Msg
+main : Program Value Model Msg
 main =
     Browser.element
         { init = init
@@ -72,8 +69,39 @@ main =
         }
 
 
-init : () -> ( Model, Cmd Msg )
+
+-- encodeQuestions : List Question -> Encode.Value
+-- encodeQuestions questionss =
+--     Encode.list questionEncoder questionss
+-- decodeQuestions : Decode.Decoder (List Question)
+-- decodeQuestions =
+--     Decode.list questionDecoder
+-- questionEncoder : Question -> Encode.Value
+-- questionEncoder question =
+--     Encode.object
+--         [ ( "text", Encode.string question.text )
+--         , ( "options", Encode.list Encode.string question.options )
+--         , ( "correctAnswer", Encode.int question.correctAnswer )
+--         ]
+-- questionDecoder : Decode.Decoder Question
+-- questionDecoder =
+--     Decode.map3 Question
+--         (Decode.field "text" Decode.string)
+--         (Decode.field "options" (Decode.list Decode.string))
+--         (Decode.field "correctAnswer" Decode.int)
+-- port saveModel : Encode.Value -> Cmd msg
+
+
+init : Value -> ( Model, Cmd Msg )
 init _ =
+    -- let
+    --     initialModel =
+    --         Decode.decodeValue Decode.string flags
+    --             |> Result.withDefault ""
+    --             |> Decode.decodeString decodeQuestions
+    --             |> Result.withDefault []
+    -- in
+    -- ( { questions = initialModel }, Cmd.none )
     ( { questions = questions }, Cmd.none )
 
 
@@ -82,40 +110,13 @@ subscriptions _ =
     Debug.todo "TODO"
 
 
-encodeQuestions : List Question -> Encode.Value
-encodeQuestions questionss =
-    Encode.list questionEncoder questionss
-
-
-decodeQuestions : Decode.Decoder (List Question)
-decodeQuestions =
-    Decode.list questionDecoder
-
-
-questionEncoder : Question -> Encode.Value
-questionEncoder question =
-    Encode.object
-        [ ( "text", Encode.string question.text )
-        , ( "options", Encode.list Encode.string question.options )
-        , ( "correctAnswer", Encode.int question.correctAnswer )
-        ]
-
-
-questionDecoder : Decode.Decoder Question
-questionDecoder =
-    Decode.map3 Question
-        (Decode.field "text" Decode.string)
-        (Decode.field "options" (Decode.list Decode.string))
-        (Decode.field "correctAnswer" Decode.int)
-
-
 view : Model -> Html Msg
 view model =
     div []
         [ div [] [ text "Quiz" ]
         , div []
             [ button [ onClick MakeQuiz ] [ text "Make Quiz" ]
-            , button [ onClick GetQuiz ] [ text "Get Quiz" ]
+            , button [] [ text "Get Quiz" ]
             ]
         , div []
             (List.indexedMap (\index question -> questionView index question) model.questions)
@@ -127,21 +128,22 @@ questionView index question =
     div [ id (String.fromInt index) ]
         [ div [] [ text question.text ]
         , div []
-            (List.indexedMap (optionView question.correctAnswer) question.options)
+            (List.map optionView question.options)
         ]
 
 
-optionView : Int -> Int -> String -> Html Msg
-optionView correctAnswer index option =
+optionView : ( String, String ) -> Html Msg
+optionView ( option1, option2 ) =
     let
         styleClass =
-            if correctAnswer == index then
-                "correct"
+            ""
 
-            else
-                "incorrect"
+        -- if correctAnswer == index then
+        --     "correct"
+        -- else
+        --     "incorrect"
     in
     div [ class styleClass ]
-        [ label [ for (String.fromInt index) ] [ text option ]
-        , input [ type_ "radio", name (String.fromInt index) ] []
+        [ label [ for option1 ] [ text option2 ]
+        , input [ type_ "radio", name option1 ] []
         ]
