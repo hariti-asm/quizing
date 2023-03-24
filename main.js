e226a72a1e9b43bde3ddca276625a9717fae7f93
Abdellah Hariti <haritiabdellah@gmail.com>
2023-03-24 00:17:40 +0000

    fix tailwind build system

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-import "./style.css";
-import { Elm } from "./src/Main.elm";
+import './src/style.build.css';
 
-if (process.env.NODE_ENV === "development") {
-  const ElmDebugTransform = await import("elm-debug-transformer");
+import { Elm } from './src/Main.elm';
+
+if (process.env.NODE_ENV === 'development') {
+  const ElmDebugTransform = await import('elm-debug-transformer');
 
   ElmDebugTransform.register({
     simple_mode: true,
   });
 }
 
-const root = document.querySelector("#app div");
+const root = document.querySelector('#app div');
 // const flags = Json.parse(localStorage.getItem("questions")) || {};
 // console.log("initilizing app with flags: ", flags);
 const app = Elm.Main.init({ node: root });
