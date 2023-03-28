4fdef6394483b5b0544fb465a4f1b9ab7c70a013
Asmae Hariti <haritiasmaa74@gmail.com>
2023-03-28 23:48:15 +0000

    adding localstorage

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-import './src/style.build.css';
+import "./src/style.build.css";
 
-import { Elm } from './src/Main.elm';
+import { Elm } from "./src/Main.elm";
 
-if (process.env.NODE_ENV === 'development') {
-  const ElmDebugTransform = await import('elm-debug-transformer');
+if (process.env.NODE_ENV === "development") {
+  const ElmDebugTransform = await import("elm-debug-transformer");
 
   ElmDebugTransform.register({
     simple_mode: true,
   });
 }
 
-const root = document.querySelector('#app div');
-// const flags = Json.parse(localStorage.getItem("questions")) || {};
-// console.log("initilizing app with flags: ", flags);
-const app = Elm.Main.init({ node: root });
-// app.ports.saveModel.subscribe(function (model) {
-//   console.log("saving model...");
-//   localStorage.setItem("questions", JSON.stringify(model));
-// });
+const root = document.querySelector("#app div");
+const flags = JSON.parse(localStorage.getItem("questions")) || {};
+console.log("initilizing app with flags: ", flags);
+const app = Elm.Main.init({ node: root, flags: flags });
+app.ports.saveModel.subscribe(function (model) {
+  console.log("saving model...");
+  localStorage.setItem("questions", JSON.stringify(model));
+});
--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,11 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import Browser
 import Html exposing (Html, button, div, input, label, span, text)
 import Html.Attributes exposing (checked, class, classList, disabled, id, name, type_, value)
 import Html.Events exposing (onClick)
-import Json.Decode exposing (Value)
+import Json.Decode as Decode exposing (Decoder, Value)
+import Json.Encode as Encode
 import Msg exposing (Msg(..))
 
 
@@ -55,6 +56,9 @@ type alias Model =
     , currentIndex : Int
     , score : Int
     }
+
+
+port saveModel : Encode.Value -> Cmd msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -108,8 +112,13 @@ update msg model =
 
                 isLastQuestion =
                     model.currentIndex == (List.length model.questions - 1)
+
+                allQuestionsAnswered =
+                    List.all
+                        (\q -> List.length q.answers > 0)
+                        model.questions
             in
-            if isLastQuestion then
+            if isLastQuestion && allQuestionsAnswered then
                 let
                     score =
                         List.foldl
@@ -140,13 +149,72 @@ main =
 
 
 init : Value -> ( Model, Cmd Msg )
-init _ =
-    ( { questions = questions, currentIndex = 0, score = 0 }, Cmd.none )
+init flags =
+    let
+        initialModel =
+            Decode.decodeValue decodeModel flags
+                |> Result.withDefault { questions = [], currentIndex = 0, score = 0 }
+    in
+    ( initialModel, saveModel (encodeModel initialModel) )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
+
+
+questionDecoder : Decoder Question
+questionDecoder =
+    Decode.map5
+        Question
+        (Decode.field "text" Decode.string)
+        (Decode.field "options" (Decode.list optionDecoder))
+        (Decode.field "correctAnswer" Decode.string)
+        (Decode.field "mark" Decode.int)
+        (Decode.field "answers" (Decode.list Decode.string))
+
+
+questionEncoder : Question -> Encode.Value
+questionEncoder question =
+    Encode.object
+        [ ( "text", Encode.string question.text )
+        , ( "options", Encode.list optionEncoder question.options )
+        , ( "correctAnswer", Encode.string question.correctAnswer )
+        , ( "mark", Encode.int question.mark )
+        ]
+
+
+optionDecoder : Decoder ( String, String )
+optionDecoder =
+    Decode.map2
+        Tuple.pair
+        (Decode.field "labdel" Decode.string)
+        (Decode.field " value " Decode.string)
+
+
+optionEncoder : ( String, String ) -> Encode.Value
+optionEncoder ( label, value ) =
+    Encode.object
+        [ ( "label", Encode.string label )
+        , ( "value", Encode.string value )
+        ]
+
+
+encodeModel : Model -> Encode.Value
+encodeModel model =
+    Encode.object
+        [ ( "questions", Encode.list questionEncoder model.questions )
+        , ( "currentIndex", Encode.int model.currentIndex )
+        , ( "score", Encode.int model.score )
+        ]
+
+
+decodeModel : Decode.Decoder Model
+decodeModel =
+    Decode.map3 Model
+        (Decode.field "questions" (Decode.list questionDecoder))
+        (Decode.field "currentIndex" Decode.int)
+        (Decode.field "score" Decode.int)
 
 
 view : Model -> Html Msg
@@ -165,7 +233,7 @@ view model =
                     model.currentIndex == (List.length model.questions - 1)
             in
             div [ class "flex  flex-col items-center mt-[200px] font-Rubik " ]
-                [ div [ class " font-bold text-3xl " ] [ text "Quiz" ]
+                [ div [ class " font-bold text-4xl " ] [ text "Quiz" ]
                 , questionView model.currentIndex
                     question
                 , div
@@ -182,7 +250,7 @@ view model =
                         button [ onClick NextQuestion, disabled (not isAnswerSelected), class "bg-[#FFC700] w-48 h-16 mt-10" ] [ text "Next" ]
                     ]
                 , if isLastQuestion then
-                    div [ class "mt-10" ]
+                    div [ class "mt-10 text-2xl font-semibold italic  " ]
                         [ text ("Your score is: " ++ String.fromInt model.score) ]
 
                   else
@@ -196,8 +264,8 @@ view model =
 questionView : Int -> Question -> Html Msg
 questionView index question =
     div [ id (String.fromInt index) ]
-        [ div [ class " text-xl mb-10" ] [ text (String.fromInt (index + 1) ++ ") " ++ question.text) ]
-        , div [ class " flex flex-col  gap-6" ]
+        [ div [ class " text-xl mb-10 text-2xl" ] [ text (String.fromInt (index + 1) ++ ") " ++ question.text) ]
+        , div [ class " flex flex-col  gap-6 text-xl" ]
             (List.map
                 (\( optionLabel, optionValue ) ->
                     viewOption
@@ -217,7 +285,7 @@ viewOption : { optionLabel : String, optionValue : String, isChecked : Bool, onS
 viewOption { optionLabel, optionValue, isChecked, onSelect } =
     label
         [ classList
-            [ ( "bg-[#8419FF] text-white", isChecked )
+            [ ( "bg-[#8419FF] text-white ", isChecked )
             , ( "border border-solid  border-1.5 solid border-[#F0EBF5]  rounded-xl gap-6 px-10 w-full py-4 focus:bg-[#8419FF]"
               , True
               )
