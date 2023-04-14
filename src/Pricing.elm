7e4887c72b1e2a1cc0c7f69c448a6f946cb3bd2d
Asmae Hariti <haritiasmaa74@gmail.com>
2023-04-14 13:52:59 +0000

    finishing with quizes page

---
+++ src/Pricing.elm
@@ -0,0 +1,1 @@
+module Pricing exposing (..)
--- src/Quizes.elm
+++ src/Quizes.elm
@@ -1,7 +1,7 @@
 module Quizes exposing (..)
 
 import Html exposing (Html, a, button, div, footer, h3, i, node, section, table, tbody, td, text, th, thead, tr)
-import Html.Attributes exposing (class, href, rel, target, type_)
+import Html.Attributes exposing (class, href, rel, scope, target, type_)
 import Html.Events exposing (onClick)
 import Msg exposing (Msg)
 
@@ -58,108 +58,63 @@ viewQuizTable quizzes =
     let
         viewQuizRow quiz =
             tr []
-                [ td [] [ text <| String.fromInt quiz.id ]
-                , td [] [ text quiz.subject ]
-                , td []
+                [ td [ class "px-6 py-4" ] [ text <| String.fromInt quiz.id ]
+                , td [ class "px-6 py-4" ] [ text quiz.subject ]
+                , td [ class "px-6 py-4" ]
                     [ button [] [ text "View" ]
                     ]
-                , td [] [ button [] [ text "Edit" ] ]
+                , td [ class " px-6 py-4" ] [ button [] [ text "Edit" ] ]
                 ]
     in
     div
-        []
-        [ section [ class "py-1 bg-blueGray-50" ]
-            [ div [ class "w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24" ]
-                [ div [ class "relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded " ]
-                    [ div [ class "rounded-t mb-0 px-4 py-3 border-0" ]
-                        [ div [ class "flex flex-wrap items-center" ]
-                            [ div [ class "relative w-full px-4 max-w-full flex-grow flex-1" ]
-                                [ h3 [ class "font-semibold text-base text-blueGray-700" ]
-                                    [ text "Quizzes" ]
-                                ]
-                            ]
-                        ]
-                    , div
-                        [ class "block w-full overflow-x-auto" ]
-                        [ table [ class "items-center bg-transparent w-full border-collapse " ]
-                            [ thead []
-                                [ tr []
-                                    [ th [ class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left" ]
-                                        [ text "Id" ]
-                                    , th
-                                        [ class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left" ]
-                                        [ text "Subject" ]
-                                    , th
-                                        [ class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left" ]
-                                        [ text "View" ]
-                                    , th
-                                        [ class " px-6 bg-blueGray-500 text-blueGray-500 align-middle border border-solid borderGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold  text-left" ]
-                                        [ text "Edit" ]
-                                    ]
-                                ]
-                            , tbody [] (List.map viewQuizRow quizzes)
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-                div
-        [ Attr.class "flex flex-col"
+        [ class "flex flex-col"
         ]
         [ div
-            [ Attr.class "overflow-x-auto sm:-mx-6 lg:-mx-8"
+            [ class "overflow-x-auto sm:-mx-6 lg:-mx-8"
             ]
             [ div
-                [ Attr.class "inline-block min-w-full py-2 sm:px-6 lg:px-8"
+                [ class "inline-block min-w-full py-2 sm:px-6 lg:px-8"
                 ]
                 [ div
-                    [ Attr.class "overflow-hidden"
+                    [ class "overflow-hidden"
                     ]
                     [ table
-                        [ Attr.class "min-w-full text-left text-sm font-light"
+                        [ class "min-w-full text-left text-sm font-light"
                         ]
                         [ thead
-                            [ Attr.class "border-b font-medium dark:border-neutral-500"
+                            [ class "border-b font-medium dark:border-neutral-500"
                             ]
                             [ tr []
                                 [ th
-                                    [ Attr.scope "col"
-                                    , Attr.class "px-6 py-4"
+                                    [ scope "col"
+                                    , class "px-6 py-4"
                                     ]
                                     [ text "#" ]
                                 , th
-                                    [ Attr.scope "col"
-                                    , Attr.class "px-6 py-4"
+                                    [ scope "col"
+                                    , class "px-6 py-4"
                                     ]
                                     [ text "Subject" ]
                                 , th
-                                    [ Attr.scope "col"
-                                    , Attr.class "px-6 py-4"
+                                    [ scope "col"
+                                    , class "px-6 py-4"
                                     ]
                                     [ text "View" ]
                                 , th
-                                    [ Attr.scope "col"
-                                    , Attr.class "px-6 py-4"
+                                    [ scope "col"
+                                    , class "px-6 py-4"
                                     ]
                                     [ text "Edit" ]
                                 ]
                             ]
-                        , tbody []
-                            [ tr
-                                [ Attr.class "border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600"
-                                ]
-                                [ td
-                                    [ Attr.class "whitespace-nowrap px-6 py-4 font-medium"
-                                    ]
-                                    [ text "2" ]
-                                ]
+                        , tbody
+                            [ class "border-b transition px-6 py-4 duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600"
                             ]
+                            (List.map viewQuizRow quizzes)
                         ]
                     ]
                 ]
             ]
-        ]
-    
         , footer
             [ class "relative pt-8 pb-6 mt-16"
             ]
@@ -175,7 +130,7 @@ viewQuizTable quizzes =
                         [ button
                             [ class "text-sm text-blueGray-500 font-semibold py-1 font-bold text-5xl"
                             ]
-                            [ text "+"
+                            [ text "Add your Quiz"
                             ]
                         ]
                     ]
