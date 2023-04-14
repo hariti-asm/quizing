54f6c8a36172c78192a176a53c2bba22ceb3a456
Asmae Hariti <haritiasmaa74@gmail.com>
2023-04-14 00:37:18 +0000

    quizes view

--- src/Quizes.elm
+++ src/Quizes.elm
@@ -1,6 +1,7 @@
 module Quizes exposing (..)
 
-import Html exposing (Html, button, div, table, tbody, td, text, th, thead, tr)
+import Html exposing (Html, a, button, div, footer, h3, i, node, section, table, tbody, td, text, th, thead, tr)
+import Html.Attributes exposing (class, href, rel, target, type_)
 import Html.Events exposing (onClick)
 import Msg exposing (Msg)
 
@@ -55,241 +56,117 @@ viewQuizTable quizzes =
                 , td [] [ button [] [ text "Edit" ] ]
                 ]
     in
-    table []
-        [ thead [] [ tr [] [ th [] [ text "ID" ], th [] [ text "Subject" ], th [] [ text "view" ], th [] [ text "Edit" ] ] ]
-        , tbody [] (List.map viewQuizRow quizes)
-        ]
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ div []
-            [ text "hello from quizes page" ]
-        , div
-            []
-            [ viewQuizTable
-                quizes
-            ]
-        ]
-        
-     {- component -}
-        node "link"
-        [ Attr.rel "stylesheet"
-        , Attr.href "https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css"
-        ]
+    -- table []
+    --     [ thead [] [ tr [] [ th [] [ text "ID" ], th [] [ text "Subject" ], th [] [ text "view" ], th [] [ text "Edit" ] ] ]
+    --     , tbody [] (List.map viewQuizRow quizes)
+    -- ]
+    div
         []
-    ,     node "link"
-        [ Attr.rel "stylesheet"
-        , Attr.href "https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css"
-        ]
-        []
-    ,     section
-        [ Attr.class "py-1 bg-blueGray-50"
-        ]
-        [ div
-            [ Attr.class "w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24"
+        [ section
+            [ class "py-1 bg-blueGray-50"
             ]
             [ div
-                [ Attr.class "relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded "
+                [ class "w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24"
                 ]
                 [ div
-                    [ Attr.class "rounded-t mb-0 px-4 py-3 border-0"
+                    [ class "relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded "
                     ]
                     [ div
-                        [ Attr.class "flex flex-wrap items-center"
+                        [ class "rounded-t mb-0 px-4 py-3 border-0"
                         ]
                         [ div
-                            [ Attr.class "relative w-full px-4 max-w-full flex-grow flex-1"
+                            [ class "flex flex-wrap items-center"
                             ]
-                            [ h3
-                                [ Attr.class "font-semibold text-base text-blueGray-700"
+                            [ div
+                                [ class "relative w-full px-4 max-w-full flex-grow flex-1"
                                 ]
-                                [ text "Page Visits" ]
-                            ]
-                        , div
-                            [ Attr.class "relative w-full px-4 max-w-full flex-grow flex-1 text-right"
-                            ]
-                            [ button
-                                [ Attr.class "bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                , Attr.type_ "button"
+                                [ h3
+                                    [ class "font-semibold text-base text-blueGray-700"
+                                    ]
+                                    [ text "Quizzes" ]
                                 ]
-                                [ text "See all" ]
-                            ]
-                        ]
-                    ]
-                , div
-                    [ Attr.class "block w-full overflow-x-auto"
-                    ]
-                    [ table
-                        [ Attr.class "items-center bg-transparent w-full border-collapse "
-                        ]
-                        [ thead []
-                            [ tr []
-                                [ th
-                                    [ Attr.class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
-                                    ]
-                                    [ text "Page name" ]
-                                , th
-                                    [ Attr.class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
-                                    ]
-                                    [ text "Visitors" ]
-                                , th
-                                    [ Attr.class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
-                                    ]
-                                    [ text "Unique users" ]
-                                , th
-                                    [ Attr.class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
-                                    ]
-                                    [ text "Bounce rate" ]
+                            , div
+                                [ class "relative w-full px-4 max-w-full flex-grow flex-1 text-right"
                                 ]
-                            ]
-                        , tbody []
-                            [ tr []
-                                [ th
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 "
+                                [ button
+                                    [ class "bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    , type_ "button"
                                     ]
-                                    [ text "/argon/" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 "
-                                    ]
-                                    [ text "4,569" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-                                    ]
-                                    [ text "340" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-                                    ]
-                                    [ i
-                                        [ Attr.class "fas fa-arrow-up text-emerald-500 mr-4"
-                                        ]
-                                        []
-                                    , text "46,53%" ]
-                                ]
-                            , tr []
-                                [ th
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700"
-                                    ]
-                                    [ text "/argon/index.html" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-                                    ]
-                                    [ text "3,985" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-                                    ]
-                                    [ text "319" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-                                    ]
-                                    [ i
-                                        [ Attr.class "fas fa-arrow-down text-orange-500 mr-4"
-                                        ]
-                                        []
-                                    , text "46,53%" ]
-                                ]
-                            , tr []
-                                [ th
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700"
-                                    ]
-                                    [ text "/argon/charts.html" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-                                    ]
-                                    [ text "3,513" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-                                    ]
-                                    [ text "294" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-                                    ]
-                                    [ i
-                                        [ Attr.class "fas fa-arrow-down text-orange-500 mr-4"
-                                        ]
-                                        []
-                                    , text "36,49%" ]
-                                ]
-                            , tr []
-                                [ th
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700"
-                                    ]
-                                    [ text "/argon/tables.html" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-                                    ]
-                                    [ text "2,050" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-                                    ]
-                                    [ text "147" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-                                    ]
-                                    [ i
-                                        [ Attr.class "fas fa-arrow-up text-emerald-500 mr-4"
-                                        ]
-                                        []
-                                    , text "50,87%" ]
-                                ]
-                            , tr []
-                                [ th
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700"
-                                    ]
-                                    [ text "/argon/profile.html" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-                                    ]
-                                    [ text "1,795" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-                                    ]
-                                    [ text "190" ]
-                                , td
-                                    [ Attr.class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
-                                    ]
-                                    [ i
-                                        [ Attr.class "fas fa-arrow-down text-red-500 mr-4"
-                                        ]
-                                        []
-                                    , text "46,53%" ]
+                                    [ text "See all" ]
                                 ]
                             ]
                         ]
-                    ]
-                ]
-            ]
-        , footer
-            [ Attr.class "relative pt-8 pb-6 mt-16"
-            ]
-            [ div
-                [ Attr.class "container mx-auto px-4"
-                ]
-                [ div
-                    [ Attr.class "flex flex-wrap items-center md:justify-between justify-center"
-                    ]
-                    [ div
-                        [ Attr.class "w-full md:w-6/12 px-4 mx-auto text-center"
+                    , div
+                        [ class "block w-full overflow-x-auto"
+                        ]
+                        [ table
+                            [ class "items-center bg-transparent w-full border-collapse "
+                            ]
+                            [ thead []
+                                [ tr []
+                                    [ th
+                                        [ class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+                                        ]
+                                        [ text "Subject" ]
+                                    , th
+                                        [ class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+                                        ]
+                                        [ text "View" ]
+                                    , th
+                                        [ class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
+                                        ]
+                                        [ text "Edit" ]
+                                    ]
+                                ]
+                            , tbody []
+                                [ tr []
+                                    [ th
+                                        [ class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 "
+                                        ]
+                                        [ text ]
+                                    , td
+                                        [ class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 "
+                                        ]
+                                        [ text "View" ]
+                                    , td
+                                        [ class "border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+                                        ]
+                                        [ text "Edit" ]
+                                    , td
+                                        [ class "border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"
+                                        ]
+                                        [ i
+                                            [ class ""
+                                            ]
+                                            []
+                                        , text ""
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    , footer
+                        [ class "relative pt-8 pb-6 mt-16"
                         ]
                         [ div
-                            [ Attr.class "text-sm text-blueGray-500 font-semibold py-1"
+                            [ class "container mx-auto px-4"
                             ]
-                            [ text "Made with", a
-                                [ Attr.href "https://www.creative-tim.com/product/notus-js"
-                                , Attr.class "text-blueGray-500 hover:text-gray-800"
-                                , Attr.target "_blank"
+                            [ div
+                                [ class "flex flex-wrap items-center md:justify-between justify-center"
                                 ]
-                                [ text "Notus JS" ]
-                            , text "by", a
-                                [ Attr.href "https://www.creative-tim.com"
-                                , Attr.class "text-blueGray-500 hover:text-blueGray-800"
-                                , Attr.target "_blank"
+                                [ div
+                                    [ class "w-full md:w-6/12 px-4 mx-auto text-center"
+                                    ]
+                                    [ div
+                                        [ class "text-sm text-blueGray-500 font-semibold py-1"
+                                        ]
+                                        [ text "Made with"
+                                        ]
+                                    ]
                                 ]
-                                [ text "Creative Tim" ]
-                            , text "." ]
+                            ]
                         ]
                     ]
                 ]
             ]
-        ]   
+        ]
