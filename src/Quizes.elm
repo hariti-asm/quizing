c22f4db623e63851201d5eff53e97f93909f00ba
hariti asmae <72623740+hariti-asm@users.noreply.github.com>
2023-04-14 12:07:39 +0000

    Update Quizes.elm

--- src/Quizes.elm
+++ src/Quizes.elm
@@ -54,52 +54,8 @@ quizes =
     ]
 
 
-viewQuizTable : List Quiz -> Html msg
 viewQuizTable quizzes =
-   
-    div
-        []
-        [ section
-            [ class "py-1 bg-blueGray-50"
-            ]
-            [ div
-                [ class "w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24"
-                ]
-                [ div
-                    [ class "relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded "
-                    ]
-                    [ div
-                        [ class "rounded-t mb-0 px-4 py-3 border-0"
-                        ]
-                        [ div
-                            [ class "flex flex-wrap items-center"
-                            ]
-                            [ div
-                                [ class "relative w-full px-4 max-w-full flex-grow flex-1"
-                                ]
-                                [ h3
-                                    [ class "font-semibold text-base text-blueGray-700"
-                                    ]
-                                    [ text "Quizzes" ]
-                                ]
-                            , div
-                                [ class "relative w-full px-4 max-w-full flex-grow flex-1 text-right"
-                                ]
-                                [ button
-                                    [ class "bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                    , type_ "button"
-                                    ]
-                                    [ text "See all" ]
-                                ]
-                            ]
-                        ]
-                    , div
-                        [ class "block w-full overflow-x-auto"
-                        ]
-                        [ table
-                            [ class "items-center bg-transparent w-full border-collapse "
-                            ]
-                             let
+    let
         viewQuizRow quiz =
             tr []
                 [ td [] [ text <| String.fromInt quiz.id ]
@@ -110,25 +66,46 @@ viewQuizTable quizzes =
                 , td [] [ button [] [ text "Edit" ] ]
                 ]
     in
+    div
+        []
+        [ section            [ class "py-1 bg-blueGray-50"            ]
+            [ div                [ class "w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24"                ]
+                [ div                    [ class "relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded "                    ]
+                    [ div                        [ class "rounded-t mb-0 px-4 py-3 border-0"                        ]
+                        [ div                            [ class "flex flex-wrap items-center"                            ]
+                            [ div                                [ class "relative w-full px-4 max-w-full flex-grow flex-1"                                ]
+                                [ h3                                    [ class "font-semibold text-base text-blueGray-700"                                    ]
+                                    [ text "Quizzes" ]
+                                ]
+                            , div
+                                [ class "relative w-full px-4 max-w-full flex-grow flex-1 text-right"                                ]
+                                [ button                                    [ class "bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"                                    , type_ "button"                                    ]
+                                    [ text "See all" ]
+                                ]
+                            ]
+                        ]
+                    , div
+                        [ class "block w-full overflow-x-auto"                        ]
+                        [ table                            [ class "items-center bg-transparent w-full border-collapse "                            ]
                             [ thead []
                                 [ tr []
-                                    [ th
-                                        [ class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
-                                        ]
-                                        [ text |> String.fromInt quiz.id ]
+                                    [ th                                        [ class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"                                        ]
+                                        [ text <| String.fromInt quiz.id ]
                                     , th
-                                        [ class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
-                                        ]
+                                        [ class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"                                        ]
                                         [ text quiz.subject ]
                                     , th
-                                        [ class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"
-                                        ]
+                                        [ class "px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left"                                        ]
                                         [ text "View" ]
                                     ]
                                 ]
-                            , tbody [] (List.map viewQuizRow quizes)
+                            , tbody [] (List.map viewQuizRow quizzes)
                             ]
                         ]
+                    ]
+                ]
+            ]
+        ]
                     , footer
                         [ class "relative pt-8 pb-6 mt-16"
                         ]
